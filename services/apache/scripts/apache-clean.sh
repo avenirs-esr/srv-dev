f09fe040ccd5c916a30fd7d38a4735a2ae01c9fe
Arnaud Deman <arnaud.deman@sppapps.com>
2023-11-28 15:27:44 +0100

    fix: pending echo in bash script

--- services/apache/scripts/apache-clean.sh
+++ services/apache/scripts/apache-clean.sh
@@ -16,7 +16,7 @@ info "Apache cleaning started."
 . $APACHE_SCRIPT_DIR/apache-env.sh $APACHE_SCRIPT_DIR 2> /dev/null \
     || err "Unable to source $APACHE_SCRIPT_DIR/apache-env.sh"
 
-echo "APACHE_ENV_FILE $PWD/$APACHE_ENV_FILE"
+
 warn_and_wait "Deleting volume root: $BOLD $AVENIRS_APACHE_VOLUMES_ROOT $NC in 4 seconds. (CtrL + C to abort)"; 
 
 sudo rm -Rf $AVENIRS_APACHE_VOLUMES_ROOT && info "$AVENIRS_APACHE_VOLUMES_ROOT deleted" || err "Unable to delete APACHE volumes root: $AVENIRS_APACHE_VOLUMES_ROOT"
