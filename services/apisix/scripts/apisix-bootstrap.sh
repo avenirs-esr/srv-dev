8fc4e671eede7b41e859097b941d1d5631d27bc4
Arnaud <arnaud.deman@univ-tln.fr>
2023-12-04 17:50:48 +0100

    feat: apisix dashboard custom image

--- services/apisix/scripts/apisix-bootstrap.sh
+++ services/apisix/scripts/apisix-bootstrap.sh
@@ -17,7 +17,7 @@ info "APISIX bootstrapping started."
 . $APISIX_SCRIPT_DIR/apisix-env.sh $APISIX_SCRIPT_DIR 2> /dev/null \
     || err "Unable to source $APISIX_SCRIPT_DIR/apisix-env.sh"
 
-# Initialization of the local branch if needed.
+# Initialization of the local branch of apisix if needed.
 cd $APISIX_REPOSITORY_DIR || err "Unable to enter $APISIX_REPOSITORY_DIR"
 if [ -z "`git branch   | grep $LOCAL_APISIX_BRANCH`" ]
 then
@@ -28,12 +28,28 @@ else
 fi
 cd - >/dev/null
 
+# git branches for APISIX and APISIX UI
+init_git_branch $APISIX_REPOSITORY_DIR $REMOTE_APISIX_BRANCH $LOCAL_APISIX_BRANCH
+init_git_branch $APISIX_UI_REPOSITORY_DIR $REMOTE_APISIX_UI_BRANCH $LOCAL_APISIX_UI_BRANCH
+# Initialization of the local branch of apisix-ui if needed.
+# cd $APISIX_REPOSITORY_DIR || err "Unable to enter $APISIX_REPOSITORY_DIR"
+# if [ -z "`git branch   | grep $LOCAL_APISIX_BRANCH`" ]
+# then
+#     verbose "Switching to branch $REMOTE_APISIX_BRANCH (local branch $LOCAL_APISIX_BRANCH)"
+#     git checkout -B $LOCAL_APISIX_BRANCH $REMOTE_APISIX_BRANCH || err "unable to create branch $LOCAL_APISIX_BRANCH from $REMOTE_APISIX_BRANCH"
+# else 
+#     verbose "Local APISIX branch found $LOCAL_APISIX_BRANCH"
+# fi
+# cd - >/dev/null
+
 # Network check
 check_network
 
 # Overlay files
 install_overlay $APISIX_OVERLAY_DIR $APISIX_REPOSITORY_DIR
 
+# .env file generation
+echo "AVENIRS_NETWORK=$AVENIRS_NETWORK" > $APISIX_ENV_FILE
 
 info "APISIX bootstrapping completed."
 
--- services/apisix/scripts/apisix-env.sh
+++ services/apisix/scripts/apisix-env.sh
@@ -4,8 +4,17 @@
 # Settings for the APISIX scripts         #
 #--------------------------------------#
 APISIX_SCRIPT_DIR=$1
-APISIX_OVERLAY_DIR=$APISIX_SCRIPT_DIR/../avenirs-apisix-overlay
+
+. $APISIX_SCRIPT_DIR/../../../scripts/srv-dev-env.sh
+
+APISIX_OVERLAY_DIR=$APISIX_SCRIPT_DIR/../avenirs-apisix-overlay/apisix-docker
 APISIX_REPOSITORY_DIR=$APISIX_SCRIPT_DIR/../apisix-docker
 REMOTE_APISIX_BRANCH=remotes/origin/release/apisix-3.6.0
 LOCAL_APISIX_BRANCH=3.6.0
+MAIN_APISIX_BRANCH="master"
+APISIX_ENV_FILE=$APISIX_REPOSITORY_DIR/example/.env
+APISIX_UI_OVERLAY_DIR=$APISIX_SCRIPT_DIR/../avenirs-apisix-overlay/apisix-dashboard
+APISIX_UI_REPOSITORY_DIR=$APISIX_SCRIPT_DIR/../apisix-dashboard
+REMOTE_APISIX_UI_BRANCH=remotes/origin/release/apisix-3.0
+LOCAL_APISIX_BRANCH=3.0
 MAIN_APISIX_BRANCH="master"
