bc7dbd231ff1f854a454ee6fe3db04a8df4b7051
Arnaud <arnaud.deman@univ-tln.fr>
2023-12-06 11:01:31 +0100

    fix: use relative path for overlay dir in docker file

--- services/apache/scripts/apache-env.sh
+++ services/apache/scripts/apache-env.sh
@@ -14,6 +14,7 @@ APACHE_ENV_FILE=$APACHE_SCRIPT_DIR/../.env
 # Docker environment 
 [ -z  "$AVENIRS_APACHE_CONTAINER_NAME" ] && AVENIRS_APACHE_CONTAINER_NAME="apache"
 [ -z  "$APACHE_OVERLAY_DIR" ] && APACHE_OVERLAY_DIR=$APACHE_SCRIPT_DIR/../avenirs-apache-overlay/
+APACHE_OVERLAY_DIRNAME=`dirname $APACHE_OVERLAY_DIR`
 
 # This is required to source this script as the test above can fail.
 return 0
