1d308643a7cab4cc0e8c79f0da02ee4aaae686be
Arnaud Deman <arnaud.deman@sppapps.com>
2023-12-12 09:26:16 +0100

    docs: comments

--- services/apisix/scripts/apisix-env.sh
+++ services/apisix/scripts/apisix-env.sh
@@ -5,8 +5,10 @@
 #--------------------------------------#
 APISIX_SCRIPT_DIR=$1
 
+# Global settings file. The settings loaded from this file are not overridden.
 . $APISIX_SCRIPT_DIR/../../../scripts/srv-dev-env.sh
 
+# Docker settings. Can be overridden in srv-dev.env.sh
 [ -z "$AVENIRS_APISIX_CONTAINER_PREFIX" ] && AVENIRS_APISIX_CONTAINER_PREFIX="${AVENIRS_CONTAINER_PREFIX}apisix-"
 [ -z "$AVENIRS_APISIX_DASHBOARD_CONTAINER_NAME" ] && AVENIRS_APISIX_DASHBOARD_CONTAINER_NAME="${AVENIRS_APISIX_CONTAINER_PREFIX}dashboard"
 [ -z "$AVENIRS_APISIX_API_CONTAINER_NAME" ] && AVENIRS_APISIX_API_CONTAINER_NAME="${AVENIRS_APISIX_API_CONTAINER_NAME}api"
@@ -14,14 +16,18 @@ APISIX_SCRIPT_DIR=$1
 [ -z "$AVENIRS_APISIX_PROMETHEUS_CONTAINER_NAME" ] && AVENIRS_APISIX_PROMETHEUS_CONTAINER_NAME="${AVENIRS_APISIX_CONTAINER_PREFIX}prometheus"
 [ -z "$AVENIRS_APISIX_GRAFANA_CONTAINER_NAME" ] && AVENIRS_APISIX_GRAFANA_CONTAINER_NAME="${AVENIRS_APISIX_CONTAINER_PREFIX}grafana"
 
-
-APISIX_OVERLAY_DIR=$APISIX_SCRIPT_DIR/../avenirs-apisix-overlay/apisix-docker
+# Git submodules.
 APISIX_REPOSITORY_DIR=$APISIX_SCRIPT_DIR/../apisix-docker
+
+# Docker env file.
+APISIX_ENV_FILE=$APISIX_REPOSITORY_DIR/example/.env
+
+# Git submodule / local / remote branches and overlay settings.
+APISIX_UI_OVERLAY_DIR=$APISIX_SCRIPT_DIR/../avenirs-apisix-overlay/apisix-dashboard
+APISIX_OVERLAY_DIR=$APISIX_SCRIPT_DIR/../avenirs-apisix-overlay/apisix-docker
 APISIX_REMOTE_BRANCH=remotes/origin/release/apisix-3.6.0
 APISIX_LOCAL_BRANCH=3.6.0
 APISIX_MAIN_BRANCH="master"
-APISIX_ENV_FILE=$APISIX_REPOSITORY_DIR/example/.env
-APISIX_UI_OVERLAY_DIR=$APISIX_SCRIPT_DIR/../avenirs-apisix-overlay/apisix-dashboard
 APISIX_UI_REPOSITORY_DIR=$APISIX_SCRIPT_DIR/../apisix-dashboard
 APISIX_UI_REMOTE_BRANCH=remotes/origin/release/3.0
 APISIX_UI_LOCAL_BRANCH=3.0
