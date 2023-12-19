f4e86861c22221a92569d7afcb46d44b0c4d122b
Arnaud Deman <arnaud.deman@sppapps.com>
2023-12-19 16:53:50 +0100

    feat: node dev api

---
+++ services/node/scripts/kafka-bootstrap.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+#--------------------------------------#
+# Kafka bootstrap                     #
+#                                      #  
+# - Docker volumes                     #
+# - Docker .env file to set            #
+#   environment from settings.         #
+#--------------------------------------#
+
+KAFKA_SCRIPT_DIR=`dirname $0`
+
+
+# Initialization
+. $KAFKA_SCRIPT_DIR/../../../scripts/srv-dev-commons.sh
+init_help "`basename $0`"
+init_commons $*
+info "Kafka bootstrapping started."
+. $KAFKA_SCRIPT_DIR/kafka-env.sh $KAFKA_SCRIPT_DIR || err "Unable to source $PWD/$KAFKA_SCRIPT_DIR/kafka-env.sh"
+
+# Network check
+check_network
+
+# .env file generation
+echo "AVENIRS_KAFKA_CONTAINER_NAME=$AVENIRS_KAFKA_CONTAINER_NAME" > $KAFKA_ENV_FILE
+echo "AVENIRS_KAFKA_VOLUMES_ROOT=$AVENIRS_KAFKA_VOLUMES_ROOT" >> $KAFKA_ENV_FILE
+echo "AVENIRS_KAFKA_UI_CONTAINER_NAME=$AVENIRS_KAFKA_UI_CONTAINER_NAME" >> $KAFKA_ENV_FILE
+echo "AVENIRS_ZOOKEEPER_CONTAINER_NAME=$AVENIRS_ZOOKEEPER_CONTAINER_NAME" >> $KAFKA_ENV_FILE
+echo "AVENIRS_ZOOKEEPER_VOLUMES_ROOT=$AVENIRS_ZOOKEEPER_VOLUMES_ROOT" >> $KAFKA_ENV_FILE
+echo "KAFKA_UI_OVERLAY_DIR=$KAFKA_UI_OVERLAY_DIR" >> $KAFKA_ENV_FILE
+echo "KAFKA_UI_OVERLAY_REALPATH=$KAFKA_UI_OVERLAY_REALPATH" >> $KAFKA_ENV_FILE
+echo "AVENIRS_NETWORK=$AVENIRS_NETWORK" >> $KAFKA_ENV_FILE
+
+
+# Volumes creation
+mkdir -p  $AVENIRS_KAFKA_DATA_VOLUME && vverbose "Volume OK: $AVENIRS_KAFKA_DATA_VOLUME" || err "Unable to create $AVENIRS_KAFKA_DATA_VOLUME"
+mkdir -p  $AVENIRS_ZOOKEEPER_DATA_VOLUME && vverbose "Volume OK: $AVENIRS_ZOOKEEPER_DATA_VOLUME" || err "Unable to create $AVENIRS_ZOOKEEPER_DATA_VOLUME"
+mkdir -p  $AVENIRS_ZOOKEEPER_LOG_VOLUME && vverbose "Volume OK: $AVENIRS_ZOOKEEPER_LOG_VOLUME" || err "Unable to create $AVENIRS_ZOOKEEPER_LOG_VOLUME"
+
+chmod o+rw $AVENIRS_KAFKA_DATA_VOLUME && verbose "Permissions on $AVENIRS_KAFKA_DATA_VOLUME OK" || err "Unable to set the permission on $AVENIRS_KAFKA_DATA_VOLUME"
+chmod o+rw $AVENIRS_ZOOKEEPER_DATA_VOLUME && verbose "Permissions on $AVENIRS_ZOOKEEPER_DATA_VOLUME OK" || err "Unable to set the permission on $AVENIRS_ZOOKEEPER_DATA_VOLUME"
+chmod o+rw $AVENIRS_ZOOKEEPER_LOG_VOLUME && verbose "Permissions on $AVENIRS_ZOOKEEPER_LOG_VOLUME OK" || err "Unable to set the permission on $AVENIRS_ZOOKEEPER_LOG_VOLUME"
+
+info "Kafka bootstrapping completed."
+
+echo "${KAFKA_UI_OVERLAY_REALPATH}/etc/kafkaui/dynamic_config.yaml"
