bfcbd42652aa256aaeba703e4117667398f435d7
Arnaud Deman <arnaud.deman@sppapps.com>
2023-12-19 17:56:51 +0100

    feat: node scripts

--- services/node/scripts/node-clean.sh
+++ services/node/scripts/node-clean.sh
@@ -1,29 +1,21 @@
 # #! /bin/bash
 
 #--------------------------------------#
-# Clean script for Kafka               #
-#                                      #  
-# Removes all the modifications:       #
-# - Deletes volumes                    #
+# Clean script for Node                #
+#                                      #
 #--------------------------------------#
 
 
 # Initialization
-KAFKA_SCRIPT_DIR=`dirname $0`
-. $KAFKA_SCRIPT_DIR/../../../scripts/srv-dev-commons.sh
+NODE_SCRIPT_DIR=`dirname $0`
+. $NODE_SCRIPT_DIR/../../../scripts/srv-dev-commons.sh
 init_commons $*
-info "Kafka cleaning started."
-. $KAFKA_SCRIPT_DIR/kafka-env.sh $KAFKA_SCRIPT_DIR 2> /dev/null \
-    || err "Unable to source $KAFKA_SCRIPT_DIR/kafka-env.sh"
+info "Node cleaning started."
+. $NODE_SCRIPT_DIR/node-env.sh $NODE_SCRIPT_DIR 2> /dev/null \
+    || err "Unable to source $NODE_SCRIPT_DIR/node-env.sh"
 
-[ -f $KAFKA_ENV_FILE ] \
-    && { rm $KAFKA_ENV_FILE && info "Docker environment file deleted: $KAFKA_ENV_FILE" || err "Unable to delete $KAFKA_ENV_FILE"; }\
-    || info "File $KAFKA_ENV_FILE not present"
+[ -f $NODE_ENV_FILE ] \
+    && { rm $NODE_ENV_FILE && info "Docker environment file deleted: $NODE_ENV_FILE" || err "Unable to delete $NODE_ENV_FILE"; }\
+    || info "File $NODE_ENV_FILE not present"
 
-warn_and_wait "Deleting volumes root: $BOLD $AVENIRS_KAFKA_VOLUMES_ROOT$NC and $BOLD $AVENIRS_ZOOKEEPER_VOLUMES_ROOT $NC in 4 seconds. (CtrL + C to abort)"; 
-
-sudo rm -Rf $AVENIRS_KAFKA_VOLUMES_ROOT && info "$AVENIRS_KAFKA_VOLUMES_ROOT deleted" || err "Unable to delete Openldap volumes root: $AVENIRS_KAFKA_VOLUMES_ROOT"
-sudo rm -Rf $AVENIRS_ZOOKEEPER_VOLUMES_ROOT && info "$AVENIRS_ZOOKEEPER_VOLUMES_ROOT deleted" || err "Unable to delete Openldap volumes root: $AVENIRS_ZOOKEEPER_VOLUMES_ROOT"
-
-
-info "Kafka cleaning completed."
+info "Node cleaning completed."
