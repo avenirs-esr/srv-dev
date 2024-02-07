8e9051d5466ac85616c8d2c9920452a80a253fc2
Arnaud <arnaud.deman@univ-tln.fr>
2024-02-07 14:01:54 +0100

    fix: typo in clean script

--- services/avenirs-api/scripts/avenirs-api-clean.sh
+++ services/avenirs-api/scripts/avenirs-api-clean.sh
@@ -11,8 +11,8 @@ AVENIRS_API_SCRIPT_DIR=`dirname $0`
 . $AVENIRS_API_SCRIPT_DIR/../../../scripts/srv-dev-commons.sh
 init_commons $*
 info "Avenirs API cleaning started."
-. $AVENIRS_API_SCRIPT_DIR/node-env.sh $AVENIRS_API_SCRIPT_DIR 2> /dev/null \
-    || err "Unable to source $AVENIRS_API_SCRIPT_DIR/node-env.sh"
+. $AVENIRS_API_SCRIPT_DIR/avenirs-api-env.sh $AVENIRS_API_SCRIPT_DIR 2> /dev/null \
+    || err "Unable to source $AVENIRS_API_SCRIPT_DIR/avenirs-api-env.sh"
 
 [ -f $AVENIRS_API_ENV_FILE ] \
     && { rm $AVENIRS_API_ENV_FILE && info "Docker environment file deleted: $AVENIRS_API_ENV_FILE" || err "Unable to delete $AVENIRS_API_ENV_FILE"; }\
