309802d8e7e5f34790092612657f39c86740dee9
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-05-13 16:11:25 +0200

    chore: sample queries for auth mock test route

---
+++ services/apisix/scripts/experiments/query-auth-mock-route-OK.curl.sh
@@ -0,0 +1,16 @@
+AT="AT-1-a29sDr2iCnLiog-RRDVpuBII5cd4WD8r"
+
+END_POINT="localhost/apim/auth-mock-test"
+
+[ "$1" = "-d" -o "$1" = "--srv-dev" ] \
+&& { END_POINT="http://srv-dev-avenir.srv-avenir.brgm.recia.net/auth-mock-test"; shift; } 
+echo -ne "\nUsing end point: $END_POINT\n\n"
+
+
+curl  -X GET  "$END_POINT" -i
+
+
+curl --header "Authorization: Bearer $AT" "$END_POINT?method=get&uri=/foo&resource=1" -i
+
+
+echo -ne "\n\n"
