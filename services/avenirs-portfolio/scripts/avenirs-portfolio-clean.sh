fab9205f49bbbe63afc5d7b5a41f0da6f3b5d77b
iamgreendev <56599296+BanelhaqB@users.noreply.github.com>
2025-05-19 17:09:22 +0200

    feat/adding-api-database (#15)
    * chore(scripts): adapt clean and bootstrap scripts for avenirs-portfolio-api database initialization
    
    * chore(deploy): remove automatic branch checkout during deploy
    
    - This caused issues when developing with sub-branches across multiple submodules.
    
    * chore(db): update changelog for v1.2.0 - initial database setup for API

--- services/avenirs-portfolio/scripts/avenirs-portfolio-clean.sh
+++ services/avenirs-portfolio/scripts/avenirs-portfolio-clean.sh
@@ -2,7 +2,7 @@
 
 #--------------------------------------#
 # Clean script for Avenirs portfolio   #
-#                                      #  
+#                                      #
 # Removes all the modifications:       #
 # - Reverts the modifications from the #
 #   repositories                       #
@@ -19,25 +19,23 @@ info "Avenirs portfolio cleaning started."
     || err "Unable to source $AVENIRS_PORTFOLIO_SCRIPT_DIR/avenirs-portfolio-env.sh"
 
 
-# ---- avenirs-portfolio-api
-
-# Resets the repository
-reset_git_repository $AVENIRS_PORTFOLIO_API_REPOSITORY_DIR $AVENIRS_PORTFOLIO_API_MAIN_BRANCH $AVENIRS_PORTFOLIO_API_LOCAL_BRANCH
-
-
 # ---- avenirs-portfolio-security
 
 # Resets the repository
 reset_git_repository $AVENIRS_PORTFOLIO_SECURITY_REPOSITORY_DIR $AVENIRS_PORTFOLIO_SECURITY_MAIN_BRANCH $AVENIRS_PORTFOLIO_SECURITY_LOCAL_BRANCH
 
 # Removes the overlay's files
-remove_overlay $AVENIRS_PORTFOLIO_SECURITY_REPOSITORY_DIR  
+remove_overlay $AVENIRS_PORTFOLIO_SECURITY_REPOSITORY_DIR
 
 # Removes the generated database sql files
-remove_file $AVENIRS_PORTFOLIO_SECURITY_CLEAN_DB_CLEAR 
+remove_file $AVENIRS_PORTFOLIO_SECURITY_CLEAN_DB_CLEAR
 remove_file $AVENIRS_PORTFOLIO_SECURITY_CLEAN_TEST_DB_CLEAR
 remove_file $AVENIRS_PORTFOLIO_SECURITY_INIT_DB_CLEAR
 remove_file $AVENIRS_PORTFOLIO_SECURITY_INIT_TEST_DB_CLEAR
+
+remove_file $AVENIRS_PORTFOLIO_API_CLEAN_DB_CLEAR
+remove_file $AVENIRS_PORTFOLIO_API_INIT_DB_CLEAR
+
 verbose "Database initialization files removed."
 
 
