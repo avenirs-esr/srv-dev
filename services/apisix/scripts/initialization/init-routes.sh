af586a8cb67f7a7b1135372e242921944cf30077
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-06-05 17:17:07 +0200

    fix: secret_env values in environment container

--- services/apisix/scripts/initialization/init-routes.sh
+++ services/apisix/scripts/initialization/init-routes.sh
@@ -3,17 +3,6 @@
 ROUTES_INIT_SCRIPT_DIR=$1
 
 
-if [ -f "/scripts/secret_env" ]; then
-    . /scripts/secret_env
-elif [ -f "$ROUTES_INIT_SCRIPT_DIR/../../secret_env" ]; then
-    . $ROUTES_INIT_SCRIPT_DIR/../../secret_env
-else
-    echo "ERROR: secret_env file not found." >&2
-    echo "This file is not versioned download it from vaultwarden or use secret_env.sample to create it." >&2
-    exit 1
-fi
-export APISIX_ADMIN_KEY=$APISIX_ADMIN_KEY
-
 echo "----"
 date
 echo "APISIX Routes initialization started."
