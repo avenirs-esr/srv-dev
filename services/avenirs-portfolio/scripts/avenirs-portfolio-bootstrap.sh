b8c8dfafe0ad6364973d0112aa62c5886147f8ad
Arnaud Deman <arnaud.deman@univ-tln.fr>
2025-12-10 17:16:36 +0100

    feat: microservice for interoperability (#70)
    * feat: microservice for interoperability added
    
    * fix: use of https in git submodule url
    
    * fix: avenirs portfolio bootstaping script

--- services/avenirs-portfolio/scripts/avenirs-portfolio-bootstrap.sh
+++ services/avenirs-portfolio/scripts/avenirs-portfolio-bootstrap.sh
@@ -18,7 +18,7 @@ write_env_file () {
   local redirection_operator="$3"
   local env_file="$4"
 
-  if "$OVERWRITE"; then
+  if [ "$OVERWRITE" = "true" ]; then
     echo "$key=$val" $redirection_operator $env_file
   else
     # Add the key only if it does not already exist
@@ -52,6 +52,12 @@ write_env_file "AVENIRS_PORTFOLIO_BACK_OFFICE_OVERLAY_DIR" "$AVENIRS_PORTFOLIO_B
 write_env_file "AVENIRS_PORTFOLIO_BACK_OFFICE_OVERLAY_BASENAME" "$AVENIRS_PORTFOLIO_BACK_OFFICE_OVERLAY_BASENAME" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
 write_env_file "AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE" "$AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
 
+write_env_file "AVENIRS_PORTFOLIO_INTEROPERABILITY_CONTAINER_NAME" "$AVENIRS_PORTFOLIO_INTEROPERABILITY_CONTAINER_NAME" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
+write_env_file "AVENIRS_PORTFOLIO_INTEROPERABILITY_CONTAINER_PORT" "$AVENIRS_PORTFOLIO_INTEROPERABILITY_CONTAINER_PORT" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
+write_env_file "AVENIRS_PORTFOLIO_INTEROPERABILITY_OVERLAY_DIR" "$AVENIRS_PORTFOLIO_INTEROPERABILITY_OVERLAY_DIR" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
+write_env_file "AVENIRS_PORTFOLIO_INTEROPERABILITY_OVERLAY_BASENAME" "$AVENIRS_PORTFOLIO_INTEROPERABILITY_OVERLAY_BASENAME" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
+write_env_file "AVENIRS_PORTFOLIO_INTEROPERABILITY_SPRING_ENV_FILE" "$AVENIRS_PORTFOLIO_INTEROPERABILITY_SPRING_ENV_FILE" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
+
 write_env_file "AVENIRS_PROMETHEUS_CONTAINER_NAME" "$AVENIRS_PROMETHEUS_CONTAINER_NAME" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
 write_env_file "AVENIRS_PROMETHEUS_CONTAINER_PORT" "$AVENIRS_PROMETHEUS_CONTAINER_PORT" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
 write_env_file "AVENIRS_GRAFANA_CONTAINER_NAME" "$AVENIRS_GRAFANA_CONTAINER_NAME" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
@@ -65,18 +71,21 @@ write_env_file "GF_SECURITY_ADMIN_PASSWORD" "$GF_SECURITY_ADMIN_PASSWORD" ">>" "
 
 # ---- avenirs-portfolio-api
 # Spring env file generation
-echo "spring.datasource.url=jdbc:postgresql://$AVENIRS_POSTGRESQL_PRIMARY_CONTAINER_NAME:5432/avenirs_api" > $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
-echo "seeder.enabled=true" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
-echo "avenirs.back-office.base-url=http://avenirs-portfolio-back-office:10010/back-office" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "spring.datasource.url" "jdbc:postgresql://$AVENIRS_POSTGRESQL_PRIMARY_CONTAINER_NAME:5432/avenirs_api" ">" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "seeder.enabled" "true" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "avenirs.back-office.base-url" "http://$AVENIRS_PORTFOLIO_BACK_OFFICE_CONTAINER_NAME:10010/back-office" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "avenirs.interoperability.base-url" "http://$AVENIRS_PORTFOLIO_INTEROPERABILITY_CONTAINER_NAME:10020/interoperability" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "avenirs.interoperability.actuator.health" "http://$AVENIRS_PORTFOLIO_INTEROPERABILITY_CONTAINER_NAME:10020/actuator/health" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+
 [ "`hostname`" = "srv-dev-avenir" ] && swagger_root="srv-dev-avenir.srv-avenir.brgm.recia.net" || swagger_root="localhost"
-echo "app.server.url=http://$swagger_root/avenirs-portfolio-api" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
-echo "logging.level.org.springframework.web=DEBUG" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
-echo "logging.level.org.springframework.web.filter.CommonsRequestLoggingFilter=DEBUG" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
-echo "logging.level.org.springframework.security=DEBUG" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
-echo "logging.level.org.springframework.security.web.FilterChainProxy=DEBUG" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
-echo "rome.4.competence.client.id=$SEC_AVENIRS_PORTFOLIO_API_ROME_4_CLIENT_ID" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
-echo "rome.4.competence.client.secret=$SEC_AVENIRS_PORTFOLIO_API_ROME_4_CLIENT_SECRET" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
-echo "security.authentication.filter=hmac" >> $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "app.server.url" "http://$swagger_root/avenirs-portfolio-api" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "logging.level.org.springframework.web" "DEBUG" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "logging.level.org.springframework.web.filter.CommonsRequestLoggingFilter" "DEBUG" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "logging.level.org.springframework.security" "DEBUG" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "logging.level.org.springframework.security.web.FilterChainProxy" "DEBUG" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "rome.4.competence.client.id" "$SEC_AVENIRS_PORTFOLIO_API_ROME_4_CLIENT_ID" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "rome.4.competence.client.secret" "$SEC_AVENIRS_PORTFOLIO_API_ROME_4_CLIENT_SECRET" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
+write_env_file "security.authentication.filter" "hmac" ">>" $AVENIRS_PORTFOLIO_API_SPRING_ENV_FILE;
 
 
 # Overlay files
@@ -85,17 +94,32 @@ install_overlay $AVENIRS_PORTFOLIO_API_OVERLAY_DIR $AVENIRS_PORTFOLIO_API_REPOSI
 
 # ---- avenirs-portfolio-back-office
 # Spring env file generation
-echo "spring.datasource.url=jdbc:postgresql://$AVENIRS_POSTGRESQL_PRIMARY_CONTAINER_NAME:5432/avenirs_back_office" > $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
-echo "seeder.enabled=true" >> $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
-echo "seeder.source=FAKER">> $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
+write_env_file "spring.datasource.url" "jdbc:postgresql://$AVENIRS_POSTGRESQL_PRIMARY_CONTAINER_NAME:5432/avenirs_back_office" ">" $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
+write_env_file "seeder.enabled" "true" ">>" $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
+write_env_file "seeder.source" "FAKER" ">>" $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
 
 [ "`hostname`" = "srv-dev-avenir" ] && swagger_root="srv-dev-avenir.srv-avenir.brgm.recia.net" || swagger_root="localhost"
-echo "app.server.url=http://$swagger_root/avenirs-portfolio-api" >> $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
-echo "security.authentication.filter=disabled" >> $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
+write_env_file "app.server.url" "http://$swagger_root/avenirs-portfolio-back-office" ">>" $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
+write_env_file  "security.authentication.filter" "disabled" ">>" $AVENIRS_PORTFOLIO_BACK_OFFICE_SPRING_ENV_FILE;
 
 # Overlay files
 echo "install_overlay $AVENIRS_PORTFOLIO_BACK_OFFICE_OVERLAY_DIR $AVENIRS_PORTFOLIO_BACK_OFFICE_REPOSITORY_DIR"
 install_overlay $AVENIRS_PORTFOLIO_BACK_OFFICE_OVERLAY_DIR $AVENIRS_PORTFOLIO_BACK_OFFICE_REPOSITORY_DIR
+
+
+# ---- avenirs-portfolio-interoperability
+# Spring env file generation
+write_env_file  "spring.datasource.url" "jdbc:postgresql://$AVENIRS_POSTGRESQL_PRIMARY_CONTAINER_NAME:5432/avenirs_interoperability" ">" $AVENIRS_PORTFOLIO_INTEROPERABILITY_SPRING_ENV_FILE;
+write_env_file  "seeder.enabled" "true" ">>" $AVENIRS_PORTFOLIO_INTEROPERABILITY_SPRING_ENV_FILE;
+write_env_file  "seeder.source" "CSV" ">>" $AVENIRS_PORTFOLIO_INTEROPERABILITY_SPRING_ENV_FILE;
+
+[ "`hostname`" = "srv-dev-avenir" ] && swagger_root="srv-dev-avenir.srv-avenir.brgm.recia.net" || swagger_root="localhost"
+write_env_file  "app.server.url" "http://$swagger_root/avenirs-portfolio-interoperability" ">>" $AVENIRS_PORTFOLIO_INTEROPERABILITY_SPRING_ENV_FILE;
+write_env_file  "security.authentication.filter" "disabled" ">>" $AVENIRS_PORTFOLIO_INTEROPERABILITY_SPRING_ENV_FILE;
+
+# Overlay files
+echo "install_overlay $AVENIRS_PORTFOLIO_INTEROPERABILITY_OVERLAY_DIR $AVENIRS_PORTFOLIO_INTEROPERABILITY_REPOSITORY_DIR"
+install_overlay $AVENIRS_PORTFOLIO_INTEROPERABILITY_OVERLAY_DIR $AVENIRS_PORTFOLIO_INTEROPERABILITY_REPOSITORY_DIR
 
 
 # ---- avernirs-cofolio-client
@@ -131,10 +155,10 @@ write_env_file "AVENIRS_PORTFOLIO_SECURITY_OVERLAY_BASENAME" "$AVENIRS_PORTFOLIO
 write_env_file "AVENIRS_PORTFOLIO_SECURITY_SPRING_ENV_FILE" "$AVENIRS_PORTFOLIO_SECURITY_SPRING_ENV_FILE" ">>" "$AVENIRS_PORTFOLIO_ENV_FILE"
 
 # Spring env file generation
-echo "spring.datasource.url=jdbc:postgresql://$AVENIRS_POSTGRESQL_PRIMARY_CONTAINER_NAME:5432/avenirs_access_control" > $AVENIRS_PORTFOLIO_SECURITY_SPRING_ENV_FILE;
+write_env_file "spring.datasource.url" "jdbc:postgresql://$AVENIRS_POSTGRESQL_PRIMARY_CONTAINER_NAME:5432/avenirs_access_control" ">" $AVENIRS_PORTFOLIO_SECURITY_SPRING_ENV_FILE;
 
 [ "`hostname`" = "srv-dev-avenir" ] && swagger_root="srv-dev-avenir.srv-avenir.brgm.recia.net" || swagger_root="localhost"
-echo "app.server.url=http://$swagger_root/avenirs-portfolio-security" >> $AVENIRS_PORTFOLIO_SECURITY_SPRING_ENV_FILE;
+write_env_file "app.server.url=http://$swagger_root/avenirs-portfolio-security" ">>" $AVENIRS_PORTFOLIO_SECURITY_SPRING_ENV_FILE;
 
 # Overlay files
 echo "install_overlay $AVENIRS_PORTFOLIO_SECURITY_OVERLAY_DIR $AVENIRS_PORTFOLIO_SECURITY_REPOSITORY_DIR"
@@ -156,8 +180,7 @@ vverbose "Created $AVENIRS_PORTFOLIO_SECURITY_INIT_DB_CLEAR"
 $JASYPT_UTIL_SCRIPT $AVENIRS_PORTFOLIO_SECURITY_INIT_TEST_DB > $AVENIRS_PORTFOLIO_SECURITY_INIT_TEST_DB_CLEAR
 vverbose "Created $AVENIRS_PORTFOLIO_SECURITY_INIT_TEST_DB_CLEAR"
 
-
-info "Avenirs portfolio security database initialization files generated."
+info "Initialization files for avenirs-portfolio-security generated."
 
 info "Database initialization files generation for avenirs-portfolio-api"
 
@@ -167,8 +190,7 @@ vverbose "Created $AVENIRS_PORTFOLIO_API_CLEAN_DB_CLEAR"
 $JASYPT_UTIL_SCRIPT $AVENIRS_PORTFOLIO_API_INIT_DB > $AVENIRS_PORTFOLIO_API_INIT_DB_CLEAR
 vverbose "Created $AVENIRS_PORTFOLIO_API_INIT_DB_CLEAR"
 
-info "Avenirs portfolio api database initialization files generated."
-
+info "Initialization files for avenirs-portfolio-api generated."
 
 
 info "Database initialization files generation for avenirs-portfolio-back-office"
@@ -179,8 +201,18 @@ vverbose "Created $AVENIRS_PORTFOLIO_BACK_OFFICE_CLEAN_DB_CLEAR"
 $JASYPT_UTIL_SCRIPT $AVENIRS_PORTFOLIO_BACK_OFFICE_INIT_DB > $AVENIRS_PORTFOLIO_BACK_OFFICE_INIT_DB_CLEAR
 vverbose "Created $AVENIRS_PORTFOLIO_BACK_OFFICE_INIT_DB_CLEAR"
 
-info "Avenirs portfolio back office database initialization files generated."
+info "Initialization files for avenirs-portfolio-back-office generated."
 
+
+info "Database initialization files generation for avenirs-portfolio-interoperability"
+
+cat $AVENIRS_PORTFOLIO_INTEROPERABILITY_CLEAN_DB  > $AVENIRS_PORTFOLIO_INTEROPERABILITY_CLEAN_DB_CLEAR
+vverbose "Created $AVENIRS_PORTFOLIO_INTEROPERABILITY_CLEAN_DB_CLEAR"
+
+$JASYPT_UTIL_SCRIPT $AVENIRS_PORTFOLIO_INTEROPERABILITY_INIT_DB > $AVENIRS_PORTFOLIO_INTEROPERABILITY_INIT_DB_CLEAR
+vverbose "Created $AVENIRS_PORTFOLIO_INTEROPERABILITY_INIT_DB_CLEAR"
+
+info "Initialization files for avenirs-portfolio-interoperability generated."
 
 . $AVENIRS_PORTFOLIO_SCRIPT_DIR/init-storage-volume.sh
 
